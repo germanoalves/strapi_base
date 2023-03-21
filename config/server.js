26cbc67d3123788c3f07e26fba271225b30d9c1f
Germano Alves <germanomacedoalves@hotmail.com>
2023-03-21 15:53:45 -0300

    version 9

--- config/server.js
+++ config/server.js
@@ -1,25 +1,15 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://strapibase-production.up.railway.app'),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'BRT_JWT_SECRET'),
+      secret: env('ADMIN_JWT_SECRET', 'xxx'),
     },
   },
-  url: env('PUBLIC_URL', 'https://strapibase-production.up.railway.app'),
-  cron: {
-    enabled: env.bool('CRON_ENABLED', true),
-  },
-  proxy: {
-    enabled: env.bool('PROXY_ENABLED', false),
-    host: env('PROXY_HOST', '127.0.0.1'),
-    port: env.int('PROXY_PORT', 8080),
-    auth: {
-      username: env('PROXY_USERNAME', ''),
-      password: env('PROXY_PASSWORD', ''),
-    },
-  },
+  keys: ['myKeyA', 'myKeyB'], // <--- Adicione essa propriedade
 });
+
 
 // module.exports = ({ env }) => ({
 //   host: env('HOST', '0.0.0.0'),
