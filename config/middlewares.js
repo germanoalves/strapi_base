b6fd0fbe820f5b65e9e42369ff527fad4776476e
Germano Alves <germanomacedoalves@hotmail.com>
2023-03-21 22:45:11 -0300

    version 14

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,34 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = {
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ["*"],
+      headers: ["*"],
+    },
+  },
+  load: {
+    before: ['cors', 'csrf', 'parser', 'public'],
+    order: [],
+    after: ['router', 'strapi::error', '404'],
+  },
+  settings: {
+    logger: {
+      level: 'debug',
+      exposeInContext: true,
+      requests: true,
+    },
+  },
+};
+
+// module.exports = [
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
